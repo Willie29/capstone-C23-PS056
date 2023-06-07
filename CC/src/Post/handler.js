dbc8870e1ca421c99090c07f898b910420e88a2d
Willyam <willdyan29@gmail.com>
2023-06-07 21:00:22 +0700

    nambah delete, get post by id, by vote, dan date

--- CC/src/Post/handler.js
+++ CC/src/Post/handler.js
@@ -1,14 +1,23 @@
 const { nanoid } = require('nanoid');
+const dayjs = require('dayjs');
 const connection = require('../dbConnect')
 
 
 const addPost = (request, h) => {
-    const { title, caption, image_url } = request.payload;
+    const { title, caption } = request.payload;
 
-    const postId = nanoid(16);
-    const createdAt = new Date().toISOString();
-    const updatedAt = createdAt;
-    const vote = 35;
+    if (!title || !caption || title.trim() === '' || caption.trim() === '') {
+      const response = h.response({
+        status: 'fail',
+        message: 'Title or caption can\'t be empty',
+      });
+      response.code(400);
+      return response;
+    };
+
+    const postId = "post" + nanoid(16);
+    const createdAt = dayjs().format('YYYY-MM-DD HH:mm:ss');
+    const vote = 12;
 
     const newPost = {
         postId,
@@ -16,7 +25,6 @@ const addPost = (request, h) => {
         caption,
         image_url,
         createdAt,
-        updatedAt,
         vote
     }
 
@@ -50,17 +58,59 @@ const getPostbyVote = (request, h) => {
   });
 };
 
-// const getPostbyDate = (request, h) => {
-//   const query = 'SELECT * from posts ORDER BY vote DESC';
-//   connection.query(query, (error, results) => {
-//     if (error) {
-//       console.error('Error retrieving posts by vote:', error);
-//       return h.response({ message: 'Error retrieving posts by vote' }).code(500);
-//     }
+const getPostbyDate = (request, h) => {
+  const query = 'SELECT * from posts ORDER BY updatedAt DESC';
+  return new Promise((resolve, reject) => {
 
-//     console.log('Posts retrieved successfully');
-//     return h.response(results).code(200);
-//   });
-// };
+    connection.query(query, (error, results) => {
 
-module.exports = { addPost, getPostbyVote }
+      if (error) {
+        console.error('Error retrieving posts by vote:', error);
+        reject(h.response({ message: 'Error retrieving posts by vote' }).code(500));
+      } 
+       
+      console.log('Posts retrieved successfully');
+      resolve(h.response(results).code(200));
+    });
+
+  });
+}
+
+const getPostbyId = (request, h) => {
+  const { postId } = request.params;
+
+  const query = 'SELECT * FROM posts WHERE postId = ?';
+  return new Promise((resolve, reject) => {
+    connection.query(query, [postId], (error, results) => {
+      if (error) {
+        console.error('Error fetching post:', error);
+        return reject(h.response({ message: 'Error fetching post' }).code(500));
+      }
+
+      if (results.length === 0) {
+        return resolve(h.response({ message: 'Post not found' }).code(404));
+      }
+
+      const post = results[0];
+      console.log('Posts retrieved successfully');
+      return resolve(h.response({ post }).code(200));
+    });
+  });
+}
+
+const deletePost = (request, h) => {
+  // const { postId } = request.params; // buat kalau udah bisa tes appnya
+  const { postId } = request.payload;
+  const query = "DELETE FROM posts WHERE `postId` = ?"
+
+  connection.query(query, postId, (error, results) => {
+    if (error) {
+      console.error('Error adding post:', error);
+      return h.response({ message: 'Error on deleting a post' }).code(500);
+    }
+  });
+  console.log('Post added successfully');
+  return h.response({ message: 'Post has been deleted' }).code(200);
+}
+
+module.exports = { addPost, getPostbyVote, getPostbyDate, deletePost, getPostbyId }
--- CC/src/routes.js
+++ CC/src/routes.js
@@ -1,15 +1,30 @@
-const { addPost, getPostbyVote } = require('./Post/handler')
+const { addPost, getPostbyVote, getPostbyDate, deletePost, getPostbyId } = require('./Post/handler')
 
 const generalRoutes = [
-    {
-        method: 'GET',
-        path: '/posts',
-        handler: getPostbyVote,
-    },
     {
         method: 'POST',
         path: '/post',
         handler: addPost,
+    },
+    {
+        method: 'GET',
+        path: '/byvote',
+        handler: getPostbyVote,
+    },
+    {
+        method: 'GET',
+        path: '/bydate',
+        handler: getPostbyDate,
+    },
+    {
+        method: 'GET',
+        path: '/post/{postId}',
+        handler: getPostbyId,
+    },
+    {
+        method: 'DELETE',
+        path: '/post',
+        handler: deletePost,
     },
 ];
  
